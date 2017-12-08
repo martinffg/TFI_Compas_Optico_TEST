1ec414d89eaf4b04d412092f013d3bc1eea3017d
martinffg <martinffg@gmail.com>
2017-12-08 18:11:10 -0300

    ant2

--- build_all.sh
+++ build_all.sh
@@ -2,8 +2,4 @@
 
 set -e
 
-mvn install:install-file -DlocalRepositoryPath=repo -DcreateChecksum=true -Dpackaging=jar -Dfile=[ufdw.jar]
-mvn install:install-file -DlocalRepositoryPath=repo -DcreateChecksum=true -Dpackaging=jar -Dfile=[jogl-all.jar]
-mvn install:install-file -DlocalRepositoryPath=repo -DcreateChecksum=true -Dpackaging=jar -Dfile=[gluegen-rt.jar]
-
-mvn clean compile test
+ant
---
+++ src/untref_tfi/view/CompassViewer.java
@@ -0,0 +1,38 @@
+package untref_tfi.view;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import untref_tfi.controller.MainGraphicInterfaceController;
+
+public class CompassViewer extends Application{
+	
+	private Scene myScene;
+	private MainGraphicInterfaceController mainGIController;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		mainGIController = new MainGraphicInterfaceController();
+				
+		myScene = mainGIController.getMainScene();		
+	
+		primaryStage.setScene(myScene);
+		primaryStage.setTitle("Compas Optico");
+		primaryStage.setWidth(1280);
+		primaryStage.setHeight(1024);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+		       @Override
+		       public void handle(WindowEvent e) {
+		          Platform.exit();
+		          System.exit(0);
+		       }
+		});
+	}
+	
+}
